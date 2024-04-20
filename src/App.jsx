dd91fd1f28260bdd11ac999219d8a19cfbb5e00e
Ritwik Bhushan <ritwik.b2001@gmail.com>
2024-04-20 23:46:14 +0530

    redux part done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-
-import './App.css'
+import { useDispatch } from "react-redux";
+import "./App.css";
+import React, { useState, useEffect } from "react";
+import authService from "./appwrite/auth";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router";
 
 function App() {
-  
-console.log(import.meta.env.VITE_APPWRITE_URL);
-  return (
-    <>
-      <h1>Blog app with appwrite
-      </h1>
-    </>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div className="min-h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header />
+        TODO: <Outlet />
+        <Footer />
+      </div>
+    </div>
+  ) : null;
 }
 
-export default App
+export default App;
--- src/conf/conf.js
+++ src/conf/conf.js
@@ -1,9 +1,9 @@
 const conf = {
-appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
-appwriteProjectId: String(import.meta.env.VITE_PROJECTID),
-appwriteDatabaseId: String(import.meta.env.VITE_DATABASEID),
-appwriteCollectionId: String(import.meta.env.VITE_COLLECTIONID),
-appwriteBucketId: String(import.meta.env.VITE_BUCKETID),
-}
+  appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+  appwriteProjectId: String(import.meta.env.VITE_PROJECTID),
+  appwriteDatabaseId: String(import.meta.env.VITE_DATABASEID),
+  appwriteCollectionId: String(import.meta.env.VITE_COLLECTIONID),
+  appwriteBucketId: String(import.meta.env.VITE_BUCKETID),
+};
 
 export default conf;
